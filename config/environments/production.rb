51984a76cb2607fba8c8f65c6c1924586286b29b
Carlos Daniel Vargas Varela <carlos.vargasvarela@ucr.ac.cr>
2024-01-29 15:40:06 -0600

    Correcciones en base de datos y production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  config.hosts << 'servicios-generales-production.herokuapp.com'
+  config.hosts << 'osgticket.so.ucr.ac.cr'
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local = false
