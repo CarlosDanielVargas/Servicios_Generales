a53d68f4613c1ccec11813b4f443f68bcd56da08
Carlos Daniel Vargas Varela <carlos.vargasvarela@ucr.ac.cr>
2024-12-02 11:10:24 -0600

    Correcciones en mensajes de correos

--- app/mailers/request_mailer.rb
+++ app/mailers/request_mailer.rb
@@ -1,5 +1,5 @@
 class RequestMailer < ApplicationMailer
-
+  helper :application # gives access to all helpers defined within `application_helper`.
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
@@ -8,7 +8,7 @@ class RequestMailer < ApplicationMailer
   def new_request(request)
     @request = request
 
-    mail to: request.requester_mail, subject: "Solicitud de servicio: #{request.identifier}"
+    mail to: request.requester_mail, subject: "Solicitud de servicio: #{request.identifier}", from: ENV["MAIL_USERNAME"]
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -19,7 +19,7 @@ class RequestMailer < ApplicationMailer
   def request_accepted(request)
     @request = request
 
-    mail to: request.requester_mail, subject: "Solicitud de servicio: #{request.identifier}"
+    mail to: request.requester_mail, subject: "Solicitud de servicio: #{request.identifier}", from: ENV["MAIL_USERNAME"]
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -31,7 +31,7 @@ class RequestMailer < ApplicationMailer
     @request = request
     @deny_reasons = RequestDenyReason.where(request_id: @request.id)
 
-    mail to: request.requester_mail, subject: "Solicitud de servicio: #{request.identifier}"
+    mail to: request.requester_mail, subject: "Solicitud de servicio: #{request.identifier}", from: ENV["MAIL_USERNAME"]
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -42,7 +42,7 @@ class RequestMailer < ApplicationMailer
   def request_completed(request)
     @request = request
 
-    mail to: request.requester_mail, subject: "Solicitud de servicio: #{request.identifier}"
+    mail to: request.requester_mail, subject: "Solicitud de servicio: #{request.identifier}", from: ENV["MAIL_USERNAME"]
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -53,6 +53,6 @@ class RequestMailer < ApplicationMailer
   def feedback_sent(request)
     @request = request
 
-    mail to: request.requester_mail, subject: "Solicitud de servicio: #{request.identifier}"
+    mail to: request.requester_mail, subject: "Solicitud de servicio: #{request.identifier}", from: ENV["MAIL_USERNAME"]
   end
 end
